da762e8d3c134e27696d30187a9962fb2ac806f5
KoukiXia <121872961+KoukiXia@users.noreply.github.com>
2025-03-03 23:32:14 +0200

    Update lseek.c

--- lseek.c
+++ lseek.c
@@ -9,17 +9,18 @@ int main (int argc, char *argv[])
     if (argc != 3) 
     {
         printf(1, "Hey, (✿◕‿◕)っ \n I just wanted to let you know that this function has the structure save <file name> <what you want appended to the file>\n ");
-        exit(1);
+        exit();
     }
 
     int thing = open(argv[1], O_CREATE | O_WRONLY);
-    lseek(thing, 0, SEEK_END);
 
     if (thing < 0) 
     {
         printf(1, "Your file is not opening. \n Nothing to do about it I guess ¯_(ツ)_/¯ \n");
-        exit(1);
+        exit();
     }
+
+    lseek(thing, 0, SEEK_END);
 
     if (strlen(argv[2]) == 0)
     {
@@ -27,7 +28,7 @@ int main (int argc, char *argv[])
         sleep(1);
         printf (1, "If you do, rewrite the instruction. \n And make sure it is in this format: \n <file name> <what you want appended to the file>\n (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ Good luck!!!\n");
         close(thing);
-        exit(0);
+        exit();
     }
 
     char space = ' ';
